113c1c440f1a3241e04244a9dd99a683fd88938f
Ademez <ademolaoladosu1@gmail.com>
2023-02-14 15:38:30 -0800

    All task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
--- lists all rows of the table second_table
--- from the database hbtn_0c_0
--- results display score and name
--- ordered by score
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- Converts `hbtn_0c_0` database to UTF8 in MySQL Server.
+
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
